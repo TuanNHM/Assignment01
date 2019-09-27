c967c7520dfcf2228eeb57b8b3ab5f2675d2a713
tuannhm <tuan.nguyen171187@gmail.com>
2019-09-28 00:07:13 +0700

    no message

--- src/test/java/modules/LoginTcs.java
+++ src/test/java/modules/LoginTcs.java
@@ -21,32 +21,27 @@ public class LoginTcs {
     @Test
     public static void loginSuccessful() throws InterruptedException {
         HomePage homepage = new HomePage(driver);
+        //Login successful
         homepage.inputUsername("standard_user");
         homepage.inputPassword("secret_sauce");
         homepage.clickLogin();
         Thread.sleep(5000);
+        System.out.println("Login Successful: Pass");
         ProductPage productpage = new ProductPage(driver);
+        //Verify accessing on Product page
         productpage.displayProductLabel();
-    }
-
-    @Test
-    public static void navigateCartPage() throws InterruptedException{
-        ProductPage productpage = new ProductPage(driver);
+        System.out.println("You are on Product page: Pass");
+        //Verify accessing on Cart page
         productpage.clickCartBtn();
-
+        Thread.sleep(5000);
         CartPage cartpage = new CartPage(driver);
         cartpage.displayCheckoutBtn();
-        Thread.sleep(5000);
-    }
-
-    @Test
-    public static void navigateProductPage() throws InterruptedException{
-        CartPage cartpage = new CartPage(driver);
+        System.out.println("You are on Cart page: Pass");
+        //Back to Product page
         cartpage.clickContinueBtn();
         Thread.sleep(5000);
-
-        ProductPage productpage = new ProductPage(driver);
         productpage.displayProductLabel();
+        System.out.println("You are back to Product page: Pass");
     }
 
     @AfterTest
